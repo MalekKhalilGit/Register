52404b96cc68451b59c49be5edf019c317a9d0f3
Malek Khalil <105339873+MalekKhalilGit@users.noreply.github.com>
2022-07-30 19:44:46 +0200

    Add files via upload

---
+++ registration-login/src/main/java/com/example/registrationlogin/web/UserRegistrationController.java
@@ -0,0 +1,35 @@
+package com.example.registrationlogin.web;
+
+import com.example.registrationlogin.model.User;
+import com.example.registrationlogin.service.UserService;
+import com.example.registrationlogin.web.dto.UserRegistrationDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/registration")
+public class UserRegistrationController {
+
+    private UserService userService;
+
+    public UserRegistrationController(UserService userService) {this.userService = userService;}
+
+    @ModelAttribute("user")
+    public UserRegistrationDto userRegistrationDto(){
+        return new UserRegistrationDto();
+    }
+
+    @GetMapping
+    public String showingRegistrationForm(){
+        return "registration";
+    }
+
+    @PostMapping
+    public String registrationUserAccount(@ModelAttribute("user")UserRegistrationDto registrationDto){
+        userService.save(registrationDto);
+        return "redirect:/registration?success";
+    }
+}
